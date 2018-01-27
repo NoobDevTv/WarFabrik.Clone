5beb207ba704edfc684f83c2cd3aecb23b6d7a1a
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-28 00:23:26 +0100

    Create Wargame Clone

---
+++ Wargame.Clone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wargame.Clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
