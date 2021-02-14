84c32cf0ee6f6efb3018b1905e2d2c8c96f416cd
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-02-14 19:28:18 +0100

    Add a simple MessageConverter
    * to enable the simple possibility to convert between received Packages to messages add this simple static  class that exactly do this.

---
+++ BotMaster.Core/Messages/MessageConvert.cs
@@ -0,0 +1,19 @@
+﻿using BotMaster.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotMaster.Core.Messages
+{
+    public static class MessageConvert
+    {
+        public static IObservable<Message> ToMessage(IObservable<Package> packages)
+            => packages.Select(p => Message.FromPackage(p));
+
+        public static IObservable<Package> ToPackage(IObservable<Message> messages)
+            => messages.Select(m => m.ToPackage());
+    }
+}
