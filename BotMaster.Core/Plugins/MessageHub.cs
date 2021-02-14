ccdd00b49618e14843f0a8eb6a68583007c411e6
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-02-14 19:32:18 +0100

    Add a simple MessageHub
    * Add a hub class that can provide a message delivery with 1 : N and 1 : 1 posibiltys. This is decided by the target id. For decubling we use a normal subject

---
+++ BotMaster.Core/Plugins/MessageHub.cs
@@ -0,0 +1,32 @@
+﻿using BotMaster.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotMaster.Core.Plugins
+{
+    public class MessageHub : IDisposable
+    {
+        private readonly Subject<Message> internalSubject;
+
+        public MessageHub()
+        {
+            internalSubject = new Subject<Message>();
+        }
+
+        public IDisposable SubscribeAsSender(IObservable<Message> messages)
+            => messages.Subscribe(internalSubject);
+
+        public IObservable<Message> SubscribeAsReceiver(string targetId)
+            => internalSubject.Where(message => string.IsNullOrEmpty(message.TargetId) || message.TargetId == targetId);
+
+        public void Dispose()
+        {
+            internalSubject.Dispose();
+        }
+    }
+}
