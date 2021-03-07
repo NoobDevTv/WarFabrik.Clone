61bc731f5a5cb9cc9ab08260d30a83969fcd871c
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-03-07 19:51:26 +0100

    Insert Logging
    * Insert a logger into the PluginHost to give the chance that someone can see the loglines an can react on Errors and get information from the logline

--- BotMaster/Program.cs
+++ BotMaster/Program.cs
@@ -1,4 +1,4 @@
-﻿using BotMaster.Runtime;
+using BotMaster.Runtime;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
@@ -21,18 +21,23 @@ namespace BotMaster
             if (!info.Directory.Exists)
                 info.Directory.Create();
 
+            using var fileTarget = new FileTarget("botmaster.logfile") { FileName = info.FullName };
 #if DEBUG
-            config.AddRule(LogLevel.Debug, LogLevel.Fatal, new ColoredConsoleTarget("botmaster.logconsole"));
-            config.AddRule(LogLevel.Trace, LogLevel.Fatal, new FileTarget("botmaster.logfile") { FileName = info.FullName });
+            using var colorTarget = new ColoredConsoleTarget("botmaster.logconsole");
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, colorTarget);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, fileTarget);
 #else
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, new FileTarget("botmaster.logfile") { FileName = info.FullName });
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, fileTarget);
 #endif
             LogManager.Configuration = config;
+
+            using var managerDispose = Disposable.Create(() => LogManager.Shutdown()); 
             var logger = LogManager.GetCurrentClassLogger();
 
             using var resetEvent = new ManualResetEvent(false);
             Console.CancelKeyPress += (s, e) => resetEvent.Set();
-            await new Service().Run();
+
+            var serviceLogger = LogManager.GetLogger($"{nameof(BotMaster)}.{nameof(Service)}");
             resetEvent.WaitOne();
         }
     }
